716e5e7ca145ce0e93124cfc617c3bb65ccfe976
JacobSerigne <JacobSerigne1@gmail.com>
2025-06-09 15:18:14 +0000

    comments

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -1,19 +1,19 @@
 var init = function (window) {
     'use strict';
     var 
-        draw = window.opspark.draw,
-        physikz = window.opspark.racket.physikz,
+        draw = window.opspark.draw, // creates the draw var
+        physikz = window.opspark.racket.physikz, // creates the physikz var
         
-        app = window.opspark.makeApp(),
-        canvas = app.canvas, 
-        view = app.view,
-        fps = draw.fps('#000');
+        app = window.opspark.makeApp(), // calls the var app
+        canvas = app.canvas, //calls the canvasvar
+        view = app.view, // calls the var view
+        fps = draw.fps('#000'); //creates number for fps
         
     
     window.opspark.makeGame = function() {
         
         window.opspark.game = {};
-        var game = window.opspark.game;
+        var game = window.opspark.game; // makes the game var equal window.opspark.game
         
         ///////////////////
         // PROGRAM SETUP //
@@ -25,7 +25,7 @@ var circles = []; // variable to store all circles in an array
 
 
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle(){
+        function drawCircle(){ //creates the drawCircle function
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2); //creates a random circle
             physikz.addRandomVelocity(circle, canvas, 5, 5); //adds a random velocity 
             view.addChild(circle);// puts the circle in view
@@ -58,7 +58,7 @@ var circles = []; // variable to store all circles in an array
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
+        function update() { //creates the update function
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
            /* physikz.updatePosition(circles[0]);
             physikz.updatePosition(circles[1]);
@@ -87,7 +87,7 @@ var circles = []; // variable to store all circles in an array
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function(circle) {
+        game.checkCirclePosition = function(circle) { //calls the function
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
@@ -111,13 +111,13 @@ var circles = []; // variable to store all circles in an array
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
         
-        view.addChild(fps);
-        app.addUpdateable(fps);
+        view.addChild(fps); //adds fps
+        app.addUpdateable(fps); //updates the fps
         
-        game.circle = circle;
-        game.circles = circles;
-        game.drawCircle = drawCircle;
-        game.update = update;
+        game.circle = circle; //makes game.circle equal circle
+        game.circles = circles; //makes game.circles equal circles
+        game.drawCircle = drawCircle; //makes game.drawCircle equal drawCircle
+        game.update = update; ///makes game.update equal update
         
         app.addUpdateable(window.opspark.game);
     }
